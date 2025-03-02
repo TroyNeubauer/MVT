51d8671a1dd24f188dbfabbb9aa7dfc785993172
Troy Neubauer <troyneubauer@gmail.com>
2025-03-02 08:13:49 -0800

    use pkgs.python3.withPackages for project

--- shell.nix
+++ shell.nix
@@ -1,26 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
-let
-  python = pkgs.python39;
-  pyPkgs = pkgs.python39Packages;
-  dyLibs = with pkgs; [stdenv.cc.cc libGL glib zlib];
-in pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ ruby_3_2 ];
-    buildInputs = with pkgs; [
-      pyPkgs.virtualenv
-    ];
-
-    shellHook = ''
-      SOURCE_DATE_EPOCH=$(date +%s)
-      VENV=".mvt39env"
-      if test ! -d $VENV; then
-        echo "creating venv!"
-        virtualenv $VENV
-      fi
-      source ./$VENV/bin/activate
-
-      export PYTHONPATH='pwd'/$VENV/${python.sitePackages}/:PYTHONPATH;
-      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath dyLibs};
-
-      pip install -e ."[dev]"
-    '';
+with pkgs; mkShell {
+  buildInputs = [
+    # Defines a python + set of packages.
+    (pkgs.python39.withPackages (ps: with ps; with python3Packages; [
+      opencv-python-headless
+      numpy
+      matplotlib
+      h5py
+      pyproj
+      scikit-image
+      ffmpeg
+      pvlib
+    ]))
+  ];
 }
